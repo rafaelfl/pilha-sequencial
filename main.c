e543d40bf0f41c01eb2ebae75a8e37288fa3b1d4
Rafael Fernandes Lopes <31193433+rafaelfl@users.noreply.github.com>
2021-08-26 23:59:53 +0000

    Realizada a implementação da validação de parênteses em expressões matemáticas

--- main.c
+++ main.c
@@ -7,22 +7,46 @@ int main(void) {
   char texto[100];
   int tamanho, i;
 
-  printf("Digite um texto a ser invertido:\n");
+  // VERIFICAR PARENTESES
+  printf("Digite uma expressão para ser verificada:\n");
 
   scanf("%[^\n]s", texto);
   tamanho = strlen(texto);
 
   for (i = 0; i < tamanho; i++) {
-    empilhar(p, texto[i]);
+    if (texto[i] == '(') {
+      empilhar(p, '(');
+    } else if (texto[i] == ')') {
+      if (desempilhar(p) == -1) {
+        printf("Parenteses DESBALANCEADOS!\n");
+        return 0;
+      }
+    }
   }
 
-  while(esta_vazia(p) == FALSE) {
-    char c = topo(p);
-    printf("%c", c);
-
-    desempilhar(p);
+  if (esta_vazia(p)) {
+    printf("Parenteses balanceados!\n");
+  } else {
+    printf("Parenteses DESBALANCEADOS!\n");
   }
-  printf("\n");
+
+  // INVERTER STRINGS
+  // printf("Digite um texto a ser invertido:\n");
+
+  // scanf("%[^\n]s", texto);
+  // tamanho = strlen(texto);
+
+  // for (i = 0; i < tamanho; i++) {
+  //   empilhar(p, texto[i]);
+  // }
+
+  // while(esta_vazia(p) == FALSE) {
+  //   char c = topo(p);
+  //   printf("%c", c);
+
+  //   desempilhar(p);
+  // }
+  // printf("\n");
   
   liberar_pilha(p);
 
